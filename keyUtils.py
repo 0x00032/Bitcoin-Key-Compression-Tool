d4ea8d195fae3400d6d58d0d999d7e3bdefa62c0
0x00032 <53447243+0x00032@users.noreply.github.com>
2022-05-11 10:27:51 -0400

    Updated: Python 3.9

--- keyUtils.py
+++ keyUtils.py
@@ -3,9 +3,8 @@ import ecdsa.der
 import ecdsa.util
 import hashlib
 import unittest
-import random
-import re
-import struct
+import codecs
+import bitcoin
 import utils
 import base58
 
@@ -14,55 +13,33 @@ import base58
 def privateKeyToWif(key_hex):    
     return base58.b58encode_check(0x80, key_hex.decode('hex'))
 
-
 def wifToPrivateKey(s):
     b = base58.bs58decode_check(s)
     return b.encode('hex')
 
-
 # Input is a hex-encoded, DER-encoded signature
-
-# Output is a 64-byte hex-encoded signature
-
+# Output is a 64-byte hex-encoded signature,
 def derSigToHexSig(s):
-    s, junk = ecdsa.der.remove_sequence(s.decode('hex'))
-    if junk != '':
-        print('JUNK', junk.encode('hex'))
-    assert(junk == '')
+    s, junk = ecdsa.der.remove_sequence(codecs.decode(s, encoding='hex'))
     x, s = ecdsa.der.remove_integer(s)
     y, s = ecdsa.der.remove_integer(s)
     return ('%064x%064x' % (x, y))
 
-
 # Input is hex string
-
 def privateKeyToPublicKey(s):
-    sk = ecdsa.SigningKey.from_string(s.decode('hex'), curve=ecdsa.SECP256k1)
-    vk = sk.verifying_key
-    return ('\04' + sk.verifying_key.to_string()).encode('hex')
-
-
+    return bitcoin.privkey_to_pubkey(s)
 
 # Input is hex string
-
 def keyToAddr(s):
-    return pubKeyToAddr(privateKeyToPublicKey(s))
-
-
+    return bitcoin.pubtoaddr(privateKeyToPublicKey(s))
 
 def pubKeyToAddr(s):
-    ripemd160 = hashlib.new('ripemd160')
-    ripemd160.update(hashlib.sha256(s.decode('hex')).digest())
-    return base58.b58encode_check(ripemd160.digest())
-
-
+    return bitcoin.pubtoaddr(s)
 
 def addrHashToScriptPubKey(b58str):
     assert(len(b58str) == 34)
-    # 76     A9      14 (20 bytes)                                 88             AC
     return ('76a914' + utils.base58CheckDecode(b58str).encode('hex') + '88ac')
     
-
 class TestKey(unittest.TestCase):
 
     def test_privateKeyToWif(self):
